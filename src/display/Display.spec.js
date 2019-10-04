5d12636ca94df7205716cd1c2cb158197c937846
lizdoyle <lizdoyle1121@gmail.com>
2019-10-03 21:14:25 -0400

    initial test for display

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -1,1 +1,13 @@
 // Test away!
+
+import React from "react";
+import { render } from "@testing-library/react";
+
+import Display from "./Display";
+
+test("it should render display", () => {
+    console.log("Default display render")
+    render(
+          <Display />
+    )
+});
